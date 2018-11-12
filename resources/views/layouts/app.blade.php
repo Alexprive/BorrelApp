50ae57c507638437bfb268c654ffd1a5f4137c62
alex poot <prive4071@gmail.com>
2018-11-12 14:49:31 +0100

    css added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ ('https://boiling-beach-35849.herokuapp.com/css/app.css') }}" rel="stylesheet">
     <style>
         .top10 {
             margin-top: 10px;
